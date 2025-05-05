ca2221b2acdeafd0f952736d3a97e5329c770aa4
Gabrielalves5 <Gabrielalves5>
2025-05-05 08:40:30 -0300

    O progama consegue executar e mostrar se o numero digitado é par ou impar

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+int numero;
+
+        Console.WriteLine("NUMERO PAR OU IMPAR?");
+        Console.Write("Digite um numero para saber se é Par ou Impar: ");
+       numero = Convert.ToInt32(Console.ReadLine());
+        if (numero %2 == 0)
+        {
+            Console.WriteLine("É par");
+        }
+        else
+            Console.WriteLine("É Impar");
+
